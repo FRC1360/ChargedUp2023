8a81ce067c2e691cf432a6931e52553aa3905310
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2023-01-18 19:43:45 -0500

    I added the code for  the piston of the claw

---
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -0,0 +1,21 @@
+package main.java.frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class Claw extends SubsystemBase {
+    
+    private Solenoid piston; // the piston to open and close the claw
+    private int solenoidChannel = 5; //Idk the piston channel so let this be the placeholder for now
+
+    public Claw() { 
+        this.piston = new Solenoid(PneumaticsModuleType.CTREPCM, solenoidChannel); 
+    }
+
+    public void set(boolean on) { 
+        piston.set(on);
+    }
+}
