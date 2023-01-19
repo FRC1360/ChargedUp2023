c3d5a2fabb7e30fc793294c3f4caae8475373780
SashaM69420 <alex.n.martynov@gmail.com>
2023-01-18 20:55:50 -0500

    align to target w/ offset

--- src/main/java/frc/robot/commands/TranslateAlign.java
+++ src/main/java/frc/robot/commands/TranslateAlign.java
@@ -10,30 +10,36 @@ public class TranslateAlign extends CommandBase {
     
     private Vision vision; 
     private DrivetrainSubsystem dt; 
+    private Vision.Pipeline pipe;
 
-    public TranslateAlign(DrivetrainSubsystem dt, Vision vision) { 
+    public TranslateAlign(DrivetrainSubsystem dt, Vision vision, Vision.Pipeline pipe) { 
         this.vision = vision; 
         this.dt = dt; 
+        this.pipe = pipe;
 
         addRequirements(dt, vision); 
     }
 
     @Override
-    public void execute() { 
+    public void initialize() {
+        vision.setPipeline(pipe);
+    }
 
+    @Override
+    public void execute() { 
         SmartDashboard.putBoolean("Align Running", true); 
 
         if (vision.hasTargets()) 
         {
-        double xOffset = vision.getX(); 
-        double yOffset = vision.getY(); 
-        double distance = vision.getDistanceFromTarget();
+            double xOffset = vision.getX(); 
+            double yOffset = vision.getY(); 
+            double distance = vision.getDistanceFromTarget();
 
-        SmartDashboard.putNumber("X-Offset:", xOffset); 
-        SmartDashboard.putNumber("Y-Offset:", yOffset);
-        SmartDashboard.putNumber("Distance:", distance);
+            SmartDashboard.putNumber("X-Offset:", xOffset); 
+            SmartDashboard.putNumber("Y-Offset:", yOffset);
+            SmartDashboard.putNumber("Distance:", distance);
 
-        dt.drive(new ChassisSpeeds(0, xOffset, 0)); 
+            dt.drive(new ChassisSpeeds(0, -xOffset/10, 0)); 
         }
     }
 
