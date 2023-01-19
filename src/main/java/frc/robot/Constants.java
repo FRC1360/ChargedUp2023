8cdc9c5dea87a34ebda8b7104d20e22b3a7df2b9
blaze <blaze@blazecoding.me>
2023-01-18 20:33:40 -0500

    Subsystem created
    Co-authored-by: superkoolkid2011 <superkoolkid2011@users.noreply.github.com>
    Co-authored-by: AahanGhode <AahanGhode@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,4 +49,7 @@ public final class Constants {
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 31; // FIXME Set back right steer motor ID
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 3; // FIXME Set back right steer encoder ID
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(132.645); // FIXME Measure and set back right steer offset
+
+    public static final int TICKS_PER_ANGLE_PIVOT = 1; // EDIT THIS TO THE ENCODER TICKS PER DEGREE
+    public static final int TICKS_PER_DISTANCE_CM = 1; // EDIT THIS TO THE ENCODER TICKS PER DISTANCE
 }
