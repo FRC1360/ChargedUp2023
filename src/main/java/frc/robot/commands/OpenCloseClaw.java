5eb460b53f0d9d60641ffa105c4fe5e014060898
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2023-01-30 18:11:16 -0500

    Changed piston to motor and tried my best to emulate the same idea. The motor will move at a certain speed until it passes a certain number of revolutions and then it will stop. This is the case for both open and close. I have no idea if my code even makes sense. All numbers are placeholders.

--- src/main/java/frc/robot/commands/OpenCloseClaw.java
+++ src/main/java/frc/robot/commands/OpenCloseClaw.java
@@ -4,15 +4,15 @@ import frc.robot.subsystems.Claw;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class OpenCloseClaw extends CommandBase{
-    private Claw piston; // that piston that open and closes the claw
+    private Claw motor; // that piston that open and closes the claw
 
-    public OpenCloseClaw(Claw piston) {
-        this.piston = piston;
-        addRequirements(piston);
+    public OpenCloseClaw(Claw motor) {
+        this.motor = motor;
+        addRequirements(motor);
     }
 
     @Override
     public void execute() {
-        piston.toggle();// toggle basically makes it do the opposite of whatever state it is in. Ex. if the claw is open it will close and vice versa. 
+        motor.toggle();       
     }
 }
--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,25 +1,55 @@
-package frc.robot.subsystems; // get rid of the main.java if this line of code is complaining
+package frc.robot.subsystems; 
 
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
+import com.revrobotics.CANSparkMax; //I don't think this is right
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 
 public class Claw extends SubsystemBase {
     
-    private Solenoid piston; // the piston to open and close the claw
-    private int solenoidChannel = 5; //Idk the piston channel so let this be the placeholder for now
+    private CANSparkMax motor; // the motor to open and close the claw
+    private int deviceId = 5; //I'm not too sure how CANsparkMax works so this part may need to be heavily modified :(
+    private RelativeEncoder encoder;
+    private double speed = 0.5;
+    private boolean status = true; //true = open false = closed
 
     public Claw() { 
-        this.piston = new Solenoid(PneumaticsModuleType.CTREPCM, solenoidChannel); 
+        this.motor = new CANSparkMax(deviceId, MotorType.kBrushless); // I don't know what I'm supposed to put here for "type" so let this be the placeholder
     }
 
-    public void set(boolean on) { 
-        piston.set(on);
+    public boolean shouldMotorStopMoving(int whenItShouldClose) { //I don't know how many counts is would take to close so placeholder. 
+        encoder = motor.getEncoder();
+        return encoder.getPosition() <= whenItShouldClose; //Basically it returns if motor passes certain # of revolution
     }
 
-    public void toggle(){
-        piston.toggle();
+    public void stop(){
+        motor.stopMotor();
     }
+
+    public void open(){
+        motor.set(speed); 
+        if (shouldMotorStopMoving(5)) { //the five is a placeholder
+            motor.stopMotor();
+        }
+    }
+
+    public void close(){
+        motor.set(-speed); //not sure if num should be - or + be it's opposite of close
+        if (shouldMotorStopMoving(5)) { //the five is a placeholder
+            motor.stopMotor();
+        }
+    }
+
+    public void toggle(){ //If claw open, it will close. If claw closed, it will open.
+        if (status) {
+            close();
+        }
+        else{
+            open();
+        }
+    }
+
+
 }
