1bb3aeaa3a30068e8dfdbd98de43c48e351c16d6
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2023-01-30 19:45:03 -0500

    I don't think the last one processed changes so I adding this one to make sure

--- src/main/java/frc/robot/commands/ClawCommand.java
+++ src/main/java/frc/robot/commands/ClawCommand.java
@@ -1,18 +1,43 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.Claw;
+import frc.robot.subsystems.ClawSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class OpenCloseClaw extends CommandBase{
-    private Claw motor; // that piston that open and closes the claw
+public class ClawCommand extends CommandBase{
+    private ClawSubsystem motor; // that piston that open and closes the claw
+    private boolean status = true; //true = open false = closed
+    private double speed = 0.5; //Also placeholder! IDK how fast the motor should be spinning.
 
-    public OpenCloseClaw(Claw motor) {
+    public ClawCommand(ClawSubsystem motor) {
         this.motor = motor;
         addRequirements(motor);
     }
 
+    public void open(){
+        motor.set(speed); 
+        if (motor.shouldMotorStopMoving(5)) { //the five is a placeholder
+            motor.stop();
+        }
+    }
+
+    public void close(){
+        motor.set(-speed); //not sure if num should be - or + be it's opposite of close
+        if (motor.shouldMotorStopMoving(5)) { //the five is a placeholder
+            motor.stop();
+        }
+    }
+
+    public void toggle(){ //If claw open, it will close. If claw closed, it will open.
+        if (status) {
+            close();
+        }
+        else{
+            open();
+        }
+    }
+
     @Override
     public void execute() {
-        motor.toggle();       
+        toggle();       
     }
 }
--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -7,16 +7,16 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 
-public class Claw extends SubsystemBase {
+public class ClawSubsystem extends SubsystemBase {
     
     private CANSparkMax motor; // the motor to open and close the claw
-    private int deviceId = 5; //I'm not too sure how CANsparkMax works so this part may need to be heavily modified :(
+    private int deviceId = 60; // 5 is placeholder number. 
     private RelativeEncoder encoder;
-    private double speed = 0.5;
-    private boolean status = true; //true = open false = closed
+    
+    
 
-    public Claw() { 
-        this.motor = new CANSparkMax(deviceId, MotorType.kBrushless); // I don't know what I'm supposed to put here for "type" so let this be the placeholder
+    public ClawSubsystem() { 
+        this.motor = new CANSparkMax(deviceId, MotorType.kBrushless);
     }
 
     public boolean shouldMotorStopMoving(int whenItShouldClose) { //I don't know how many counts is would take to close so placeholder. 
@@ -28,28 +28,8 @@ public class Claw extends SubsystemBase {
         motor.stopMotor();
     }
 
-    public void open(){
-        motor.set(speed); 
-        if (shouldMotorStopMoving(5)) { //the five is a placeholder
-            motor.stopMotor();
-        }
+    public void set(double speed){
+        motor.set(speed);
     }
-
-    public void close(){
-        motor.set(-speed); //not sure if num should be - or + be it's opposite of close
-        if (shouldMotorStopMoving(5)) { //the five is a placeholder
-            motor.stopMotor();
-        }
-    }
-
-    public void toggle(){ //If claw open, it will close. If claw closed, it will open.
-        if (status) {
-            close();
-        }
-        else{
-            open();
-        }
-    }
-
 
 }
