6257c78802bde26a248edca07e30ec073544ffc9
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2023-02-04 14:44:49 -0500

    stuff

--- src/main/java/frc/robot/commands/AutoIntakeCommand.java
+++ src/main/java/frc/robot/commands/AutoIntakeCommand.java
@@ -5,52 +5,34 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class AutoIntakeCommand extends CommandBase{
     private IntakeSubsystem intake; // that piston that open and closes the claw
-    private double speed = 0.5; //Also placeholder! IDK how fast the motor should be spinning.
-    private int whenItShouldClose = 5;
+    private double speed; 
 
-    public AutoIntakeCommand(IntakeSubsystem intake) {
+    public AutoIntakeCommand(IntakeSubsystem intake, double speed) {
         this.intake = intake;
+        this.speed = speed;
         addRequirements(intake);
-        withInterruptBehavior(InterruptionBehavior.kCancelSelf); //makes it so telop takes priority to auto command.
     }
 
-    public void intake(){
-        intake.activate(speed); 
-        if (intake.shouldMotorStopMoving(whenItShouldClose)) { //the five is a placeholder
-            intake.stop();
-        }
-    }
-
-    public void putDown(){
-        intake.activate(-speed); //not sure if num should be - or + be it's opposite of close
-        if (intake.shouldMotorStopMoving(whenItShouldClose)) { //the five is a placeholder
-            intake.stop();
-        }
-    }
-
-    public boolean isRobotReadyToDropOff(){
-        return true; // Placeholder for the sensor that will tell the robot wether it is ready to putdown object
-    }
 
     @Override
     public void initialize(){
-        intake();
+  
         
     }
 
     @Override
     public void execute() {      
-
+        intake.intake(speed);
     }
 
     @Override
     public void end(boolean interruptible) { 
-        putDown();
     }
 
     @Override
     public boolean isFinished() { 
-        return isRobotReadyToDropOff();
+        intake.stop();
+        return true;
     }
 
 }
