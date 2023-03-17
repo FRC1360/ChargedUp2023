70589b99121df43602d72da35e368b293ca8d69e
blaze <Arnnav0kudale@gmail.com>
2023-03-17 10:33:18 -0400

    Created Arm Home Command

---
+++ src/main/java/frc/robot/commands/arm/ArmHomeCommand.java
@@ -0,0 +1,61 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.ArmSubsystem;
+
+
+public class ArmHomeCommand extends CommandBase {
+
+    private ArmSubsystem arm;
+    private boolean homed;
+
+
+    private DigitalInput limitSwitch;
+
+    public ArmHomeCommand(ArmSubsystem arm) {
+            this.arm = arm;
+            this.homed = false;
+            addRequirements(arm);
+    }
+
+    @Override
+    public void initialize() {
+        this.limitSwitch = new DigitalInput(Constants.LIMIT_SWITCHES.ARM);
+    }
+
+    @Override
+    public void execute() {
+        arm.setArmSpeed(-0.25);
+        if (this.limitSwitch.get()) {
+            if (!this.limitSwitch.get()) {
+                arm.setArmSpeed(0);
+                return;
+            }
+        }
+        
+        arm.resetEncoder();
+        arm.setArmSpeed(0.1);
+
+        if (!this.limitSwitch.get()) {
+            if (this.limitSwitch.get()) {
+                arm.setArmSpeed(0);
+                return;
+            }
+        }
+
+        arm.resetEncoder();
+        homed = true;
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return homed;
+        //return false; 
+    }
+
+
+    
+}
