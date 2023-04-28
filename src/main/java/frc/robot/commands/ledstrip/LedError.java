159a844761ff9d11ee367e5eff98b3c6d0d3de3a
blaze <Arnnav0kudale@gmail.com>
2023-04-28 17:31:32 -0400

    Created LED Error command to use for errors on the robot

---
+++ src/main/java/frc/robot/commands/ledstrip/LedError.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.shoulder;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LEDSubsystem;
+
+public class LedError extends CommandBase {
+
+    LEDSubsystem led;
+
+    public LedError(LEDSubsystem led) {
+        this.led = led;
+
+        addRequirements(led);
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        led.setLEDCode(-0.35);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
