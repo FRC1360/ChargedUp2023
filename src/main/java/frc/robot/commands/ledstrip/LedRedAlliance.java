d57ef1f4a1099a8d9c660bb5c6eb139f7584cd5d
blaze <Arnnav0kudale@gmail.com>
2023-04-29 10:15:54 -0400

    Added Red and Blue Alliance LED commands

---
+++ src/main/java/frc/robot/commands/ledstrip/LedRedAlliance.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.shoulder;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LEDSubsystem;
+
+public class LedBlueAlliance extends CommandBase {
+
+    LEDSubsystem led;
+
+    public LedBlueAlliance(LEDSubsystem led) {
+        this.led = led;
+
+        addRequirements(led);
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        led.setLEDCode(-0.17);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
