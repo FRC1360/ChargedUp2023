349a30e96637ad9eb6eb84da791d454c408dbca8
blaze <Arnnav0kudale@gmail.com>
2023-08-10 23:24:02 -0400

    Started mapping panel, left throttle done

---
+++ src/main/java/frc/robot/util/HOTASPanel.java
@@ -0,0 +1,74 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class HOTASController extends GenericHID {
+    private static final int L_ROLL_IN = 1;
+    private static final int L_ROLL_FORWARD_CW = 2;
+    private static final int L_ROLL_FORWARD_CCW = 3;
+
+    private static final int L_PRESS = 4;
+
+    private static final int L_TOGGLE_PRESS = 5;
+    private static final int L_TOGGLE_FORWARD = 6;
+    private static final int L_TOGGLE_BACKWARD = 7;
+
+    private static final int R_ROLL_IN = 1;
+
+    // TODO check the acc values from driver station and shove their values in here.
+    // i prolly need to make more and map like 20 buttons.
+
+    private static final int AXIS_X = 0;
+    private static final int AXIS_Y = 1;
+
+    private Joystick joystick;
+
+    public HOTASController(int port) {
+        joystick = new Joystick(port);
+    }
+
+    @Override
+    public double getX(Hand hand) {
+        if (hand.equals(Hand.kRight)) {
+            return joystick.getRawAxis(AXIS_X);
+        }
+        return 0.0;
+    }
+
+    @Override
+    public double getY(Hand hand) {
+        if (hand.equals(Hand.kRight)) {
+            return joystick.getRawAxis(AXIS_Y);
+        }
+        return 0.0;
+    }
+
+    public boolean getTriggerHalfButton() {
+        return joystick.getRawButton(BUTTON_TRIGGER_HALF);
+    }
+
+    public boolean getTriggerFullButton() {
+        return joystick.getRawButton(BUTTON_TRIGGER_FULL);
+    }
+
+    public boolean getThumbButton() {
+        return joystick.getRawButton(BUTTON_THUMB);
+    }
+
+    public boolean getFireAButton() {
+        return joystick.getRawButton(BUTTON_FIRE_A);
+    }
+
+    public boolean getPrimaryButton() {
+        return joystick.getRawButton(BUTTON_PRIMARY);
+    }
+
+    public boolean getSecondaryButton() {
+        return joystick.getRawButton(BUTTON_SECONDARY);
+    }
+
+    public double getTwistAxis() {
+        return joystick.getRawAxis(AXIS_TWIST);
+    }
+}
