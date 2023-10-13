6399afe4afa103147634fc2182478e0a51f2bd37
WillWye <145507796+WillWye@users.noreply.github.com>
2023-10-12 20:33:18 -0400

    Test
    Testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,15 +14,7 @@ import frc.robot.subsystems.IntakeSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-
-  private RobotContainer m_robotContainer;
-
+ * 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
